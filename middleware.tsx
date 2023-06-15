6ba555736956538f43d3b2f3741b727529bea1b9
Mert Osanmaz <mosanmaz@danusports.com>
2023-06-15 09:25:33 +0100

    relavoux.com.tr redirect

--- middleware.tsx
+++ middleware.tsx
@@ -11,7 +11,7 @@ export async function middleware(req: NextRequest) {
     console.log(country)
     // If the user is from Turkey, redirect them to a different URL
     if (country === 'TR') {
-        return NextResponse.redirect('https://moshapparel.com');
+        return NextResponse.redirect('https://relavoux.com.tr');
     }
 
     url.searchParams.set('country', country);
